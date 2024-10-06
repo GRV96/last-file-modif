da428db9127133d09e743e6c30b12485579393ab
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-10-06 12:09:38 -0400

    The modification moments are vertically aligned.

--- __main__.py
+++ __main__.py
@@ -3,10 +3,28 @@ This command line application displays the name and the moment of the last
 modification of each item in the specified directory.
 """
 
+
 from datetime import datetime
 from pathlib import Path
 from argparse import ArgumentParser
 from sys import exit
+
+
+_DOT = "."
+
+
+class LastModifOutput:
+	def __init__(self, name, moment):
+		self._name = name
+		self._moment = moment
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def moment(self):
+		return self._moment
 
 
 def _make_arg_parser():
@@ -40,10 +58,25 @@ if not directory.is_dir():
 	print("-d/--direcotry: A direcotry is expected.")
 	exit(1)
 
+outputs = list()
+greatest_length = 0
 for item in directory.glob("*"):
 	last_modif_timestamp = item.stat().st_mtime
 	last_modif_moment = datetime.fromtimestamp(last_modif_timestamp)
 	last_modif_strf = last_modif_moment.isoformat()
 
 	item = _rel_path_with_nb_parents(item, nb_parents)
-	print(f"{item}....{last_modif_strf}")
+	item_str = str(item)
+	name_length = len(item_str)
+	outputs.append(LastModifOutput(item_str, last_modif_strf))
+
+	if name_length > greatest_length:
+		greatest_length = name_length
+
+greatest_length += 4
+for output in outputs:
+	name = output.name
+	name_length = len(name)
+	nb_dots = greatest_length - name_length
+
+	print(f"{name}{_DOT * nb_dots}{output.moment}")
